ea587c9b086ba8c8f5a637df6778c0ce431d79e1
vishalasware85 <154328153+vishalasware85@users.noreply.github.com>
2024-01-09 23:37:32 +0530

    Update TodoDBHelper.java

--- app/src/main/java/com/todolistapp/database/TodoDBHelper.java
+++ app/src/main/java/com/todolistapp/database/TodoDBHelper.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by hitanshu on 19/7/17.
- */
-
 public class TodoDBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "database.db";
